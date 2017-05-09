87fd50be42e8dcd9b5c039b9c3ffcd9247959892
lajonss <gfs.b16@gmail.com>
2017-05-09 12:13:10 +0200

    upd

--- src/main/java/visualization/InterfaceController.java
+++ src/main/java/visualization/InterfaceController.java
@@ -3,11 +3,12 @@ package visualization;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Map;
+import org.springframework.ui.Model;
 
 @Controller
 public class InterfaceController {
     @RequestMapping("/")
-    public String index(Map<String, Object> model) {
-	return "/webapp/index.html";
+    public String index(Model model) {
+	return "index";
     }
 }
