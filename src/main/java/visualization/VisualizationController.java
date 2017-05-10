8ea7fcb1bcd2d34f8a50e8fe244e25cefb3318ff
lajonss <gfs.b16@gmail.com>
2017-05-10 03:06:31 +0200

    cors

--- src/main/java/visualization/VisualizationController.java
+++ src/main/java/visualization/VisualizationController.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="*")
 @RestController
 public class VisualizationController {
     @Autowired
