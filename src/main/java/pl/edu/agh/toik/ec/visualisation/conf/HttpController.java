a4d00fb5c60cf6c2ee7c5a9a1ffaf0fa6e71db6f
lajonss <gfs.b16@gmail.com>
2017-05-30 12:25:06 +0200

    config endpoint

--- src/main/java/pl/edu/agh/toik/ec/visualisation/conf/HttpController.java
+++ src/main/java/pl/edu/agh/toik/ec/visualisation/conf/HttpController.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.toik.ec.visualisation.conf;
 
+import java.io.Serializable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pl.edu.agh.toik.ec.visualisation.Visualization;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
 public class HttpController {
 	@Autowired
 	private Visualization visualization;
-	
-	@RequestMapping("/type")
-	public String getVisualizationType() {
-		return visualization.getType().getClass().getSimpleName();
+
+	@RequestMapping("/config")
+	public ConfigEndpoint getVisualizationType() {
+		ConfigEndpoint ce = new ConfigEndpoint();
+		ce.type = visualization.getType().getClass().getSimpleName();
+		ce.biggerFitnessIsBetter = true; // TODO link with algorithm module
+		return ce;
+	}
+
+	private class ConfigEndpoint implements Serializable {
+		public String type;
+		public Boolean biggerFitnessIsBetter;
 	}
 }
